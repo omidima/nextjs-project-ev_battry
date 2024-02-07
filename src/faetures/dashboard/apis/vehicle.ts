3de03f3da8b9c3d49abd53432a7621d2b7aa698c
امید حدیدی <o.h.hadidy@gmail.com>
2024-02-07 14:45:41 +0330

    Changes to be committed: 	modified:   src/faetures/dashboard/apis/vehicle.ts

--- src/faetures/dashboard/apis/vehicle.ts
+++ src/faetures/dashboard/apis/vehicle.ts
@@ -6,6 +6,6 @@ const api = API.getInstance()
 
 export async function getGetVehicles() {
     return api.GetMethod<VehicleDto[]>({
-        url: "https://nextjs-project-ev-battry-rfaez4fxe-omidima.vercel.app/api/vehicle-status"
+        url: "https://nextjs-project-ev-battry.vercel.app/api/vehicle-status"
     })
 }
