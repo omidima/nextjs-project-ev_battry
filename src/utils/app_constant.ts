10256b1b6e3cdcd40c027f1a10d74ae9026dbbf0
امید حدیدی <o.h.hadidy@gmail.com>
2024-02-08 15:31:54 +0330

    Change public url

--- src/utils/app_constant.ts
+++ src/utils/app_constant.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL ?? "https://nextjs-project-ev-battry-rfaez4fxe-omidima.vercel.app"
+export const API_URL = process.env.API_URL ?? "https://nextjs-project-ev-battry.vercel.app"
 // export const API_URL = process.env.API_URL ?? "http://localhost:3000"
