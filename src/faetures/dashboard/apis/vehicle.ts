f46ae7542efad6ff6728d4e8acdb6afd6cf95aca
امید حدیدی <o.h.hadidy@gmail.com>
2024-03-16 18:21:15 +0330

    Change request address

--- src/faetures/dashboard/apis/vehicle.ts
+++ src/faetures/dashboard/apis/vehicle.ts
@@ -2,10 +2,10 @@ import parseSdk from "@/core/network/parse";
 import { VehicleDto } from "../types/vehicle.dto";
 
 export async function getGetVehicles() : Promise<VehicleDto[]> {
-    // const ratings = await parseSdk.Cloud.run("battery_data", {});
-    // return ratings["battery_data"]
+    const ratings = await parseSdk.Cloud.run("battery_data", {});
+    return ratings["battery_data"]
 
-    return (await (await fetch("/api/vehicle-status")).json())["result"]["battery_data"]
+    // return (await (await fetch("/api/vehicle-status")).json())["result"]["battery_data"]
 }
 
 export async function connectNewVehicle(url: string) {
